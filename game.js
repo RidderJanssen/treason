c6beba96a39489545159d9c659999e31b7b828ea
RidderJanssen <ridderjanssen@gmail.com>
2020-07-29 20:38:25 +0200

    Hopefully this is the last change to make everything functional: when a player gets a new turn they automatically get a card.

--- game.js
+++ game.js
@@ -1332,6 +1332,9 @@ module.exports = function createGame(options) {
             });
             gameTracker.startOfTurn(state);
         }
+        // Give the current player a new card
+        playerIdx = state.state.playerIdx
+        state.players[playerIdx].influences[0].role = deck.pop();
     }
 
     function indexOfInfluence(playerState, role) {
